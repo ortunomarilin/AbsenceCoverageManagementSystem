29182730bde7977b596b41c6a4047a57cea6a4f5
ortuno.marilin@outlook.com <ortuno.marilin@outlook.com>
2020-03-24 22:11:19 -0500

    Added IRepository and Repository classes.

--- AbsenceCoverageMS/Controllers/HomeController.cs
+++ AbsenceCoverageMS/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using AbsenceCoverageMS.Models.DataLayer;
 using AbsenceCoverageMS.Models.DomainModels;
-using AbsenceCoverageMS.Models.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
---
+++ AbsenceCoverageMS/Models/DataLayer/Repositories/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AbsenceCoverageMS.Models.DataLayer.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        List<TEntity> GetAll();
+        
+        TEntity Get(string id);
+
+        void Add(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Delete(TEntity entity);
+
+        void Save();
+
+
+
+
+    }
+}
