db9de6043b5381a2a39e352b2079daf4091b4db2
ortuno.marilin@outlook.com <ortuno.marilin@outlook.com>
2020-03-25 04:42:18 -0500

    Created basic LogIn View. Plus, corrected bootstrap link for the main Layout and LogIn Views.

--- AbsenceCoverageMS/Areas/Admin/Controllers/HomeController.cs
+++ AbsenceCoverageMS/Areas/Admin/Controllers/HomeController.cs
@@ -2,15 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using AbsenceCoverageMS.Models.DataLayer;
-using AbsenceCoverageMS.Models.DomainModels;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
-namespace AbsenceCoverageMS.Controllers
+namespace AbsenceCoverageMS.Areas.Admin.Controllers
 {
-    [Authorize]
+    [Authorize(Roles = "Admin")]
+    [Area("Admin")]
     public class HomeController : Controller
     {
         public IActionResult Index()
